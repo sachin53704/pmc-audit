a410dde8a5992b325fcd1c9fb8367d787f06ff05
coreoceantesting <coreoceantesting@gmail.com>
2024-08-08 14:52:51 +0530

    chnage select box class name

--- app/Http/Controllers/Admin/AccountPaymentReceiptController.php
+++ app/Http/Controllers/Admin/AccountPaymentReceiptController.php
@@ -442,7 +442,7 @@ class AccountPaymentReceiptController extends Controller
 
                     <div class="col-md-2 mt-3">
                         <label class="col-form-label" for="action_' . $key . '">DY Auditor Action</label>
-                        <select name="' . ($roleName == "DY Auditor" ? $actionFieldName : "") . '" ' . ($roleName == "DY Auditor" ? $isEditable : "readonly") . ' class="form-control">
+                        <select name="' . ($roleName == "DY Auditor" ? $actionFieldName : "") . '" ' . ($roleName == "DY Auditor" ? $isEditable : "readonly") . ' class="form-select">
                             <option value="">Action</option>
                             <option value="1" ' . ($subreceipt->dy_auditor_status == 1 ? "selected" : "") . '>Approve</option>
                             <option value="2" ' . ($subreceipt->dy_auditor_status == 2 ? "selected" : "") . '>Reject</option>
@@ -457,7 +457,7 @@ class AccountPaymentReceiptController extends Controller
 
                     <div class="col-md-2 mt-3">
                         <label class="col-form-label" for="action_' . $key . '">DY MCA Action</label>
-                        <select name="' . ($roleName == "DY MCA" ? $actionFieldName : "") . '" class="form-control" ' . ($roleName == "DY MCA" ? $isEditable : "readonly") . '>
+                        <select name="' . ($roleName == "DY MCA" ? $actionFieldName : "") . '" class="form-select" ' . ($roleName == "DY MCA" ? $isEditable : "readonly") . '>
                             <option value="">Action</option>
                             <option value="1" ' . ($subreceipt->dy_mca_status == 1 ? "selected" : "") . '>Approve</option>
                             <option value="2" ' . ($subreceipt->dy_mca_status == 2 ? "selected" : "") . '>Reject</option>
@@ -472,7 +472,7 @@ class AccountPaymentReceiptController extends Controller
 
                     <div class="col-md-2 mt-3">
                         <label class="col-form-label" for="action_' . $key . '">MCA Action</label>
-                        <select name="' . ($roleName == "MCA" ? $actionFieldName : "") . '" class="form-control" ' . ($roleName == "MCA" ? $isEditable : "readonly") . '>
+                        <select name="' . ($roleName == "MCA" ? $actionFieldName : "") . '" class="form-select" ' . ($roleName == "MCA" ? $isEditable : "readonly") . '>
                             <option value="">Action</option>
                             <option value="1" ' . ($subreceipt->mca_status == 1 ? "selected" : "") . '>Approve</option>
                             <option value="2" ' . ($subreceipt->mca_status == 2 ? "selected" : "") . '>Reject</option>
